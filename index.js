8e5a532eff7fac62a268096de90c6465c0ed18e3
Kunal Kalita <kunal.kalita54@gmail.com>
2026-09-16 19:09:56 +0530

    Organize project into backend and frontend

--- index.js
+++
@@ -1,9 +0,0 @@
-import app from './src/app.js';
-import connectDB from './src/config/database.js';
-
-
-connectDB();
-
-app.listen(3000, ()=> {
-    console.log('server is running at port 3000');
-})
